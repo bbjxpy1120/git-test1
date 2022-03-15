57c5b2fc9251736f5584b4f925e40028b515d976
Lish <Lish@lsh.com>
2022-03-15 15:25:29 +0800

    first commit

---
+++ src/main/java/com/lsh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lsh.git;
+
+/**
+ * @author shkstart
+ * @create 2022-03-15-15:21
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
